4d31720488f7c77287650c16ec4be9f47cfb1248
柴士童 <836275709@qq.com>
2019-12-29 00:49:22 +0100

    mute error message, trim useless code

--- svm_torch.py
+++ svm_torch.py
@@ -62,8 +62,8 @@ class svm_model_torch:
                             H = min(self.C, (a2_old + a1_old).item())
                             L = max(0, (a2_old + a1_old - self.C).item())
                         
-                        E1 = self.error_k(k, x1, y1,x,y)
-                        E2 = self.error_k(k, x2, y2,x,y)
+                        E1 = self.error_k(k, x1, y1,x)
+                        E2 = self.error_k(k, x2, y2,x)
                         dx = x1 - x2
                         kappa = self.kernel(dx,dx)
                         delta = y2 * (E1-E2)/kappa
@@ -73,8 +73,8 @@ class svm_model_torch:
                         
                         a1_new = self.a[k,i]
                         a2_new = self.a[k,i+1]
-                        b1 = y1 - self.g_k_nobias(k, x1, x, y)
-                        b2 = y2 - self.g_k_nobias(k, x2, x, y)
+                        b1 = y1 - self.g_k_nobias(k, x1, x)
+                        b2 = y2 - self.g_k_nobias(k, x2, x)
                         if 0<a1_new<self.C:
                             self.b[k,0] = b1
                         elif 0<a2_new<self.C:
@@ -89,8 +89,7 @@ class svm_model_torch:
         k_predicts = torch.zeros((self.n_svm, n_x))
         for k in range(self.n_svm):
             for i in range(n_x):
-                y = self.cast(self.y_multiclass, k)
-                k_predicts[k, i] = self.g_k(k, x[i,:].view(1,-1), self.x, y)
+                k_predicts[k, i] = self.g_k(k, x[i,:].view(1,-1), self.x)
         result = torch.argmax(torch.matmul(self.lookup_matrix, k_predicts ),axis=0)
         return result.reshape(-1,1)
         
@@ -99,19 +98,18 @@ class svm_model_torch:
         # the pos/neg (with 0) where pos/neg are what SVMk concerns
         return (y==self.lookup_class[k][0]).float() - (y==self.lookup_class[k][1]).float()
         
-    def g_k_nobias(self, k, xi, x, y):
+    def g_k_nobias(self, k, xi, x):
         # The prediction of SVMk
-#         y = self.cast(y_multiclass, k)
+        y = self.cast(self.y_multiclass, k)
         a = self.a[k,:].view(-1,1)
         gx = (y * a) * self.kernel(xi, x) 
-        print(y.shape,a.shape,self.kernel(xi, x).shape)
         return torch.sum(gx)
     
-    def g_k(self,k,xi,x,y):
-        return self.g_k_nobias(k,xi,x,y) + self.b[k,0]
+    def g_k(self,k,xi,x):
+        return self.g_k_nobias(k,xi,x) + self.b[k,0]
     
-    def error_k(self, k, xi, yi, x, y):
-        return self.g_k(k,xi,x,y)-yi
+    def error_k(self, k, xi, yi, x):
+        return self.g_k(k,xi,x)-yi
     
     def get_w(self, k):
         y = self.cast(self.y_multiclass, k)
