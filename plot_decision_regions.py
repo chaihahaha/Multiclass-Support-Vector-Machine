0e47df53dd29d64414a93170679e5cec696b9316
柴士童 <836275709@qq.com>
2020-01-07 22:17:18 +0100

    Create plot_decision_regions.py

---
+++ plot_decision_regions.py
@@ -0,0 +1,29 @@
+from svm_pytorch import svm_model_torch
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+import numpy as np
+colors = ['red','green','blue','purple']
+data_x = np.array([[-2,1],[-2,2],[-1,1],[-1,2],[1,1],[1,2],[2,1],[2,2],[1,-1],[1,-2],[2,-1],[2,-2]])
+data_y = np.array([[0],[0],[0],[0],[1],[1],[1],[1],[2],[2],[2],[2]]).reshape(-1)
+fig = plt.figure()
+fig = plt.scatter(data_x[:,0],data_x[:,1],c=data_y, cmap=ListedColormap(colors))
+m = len(data_x)
+c = len(np.unique(data_y))
+svm = svm_model_torch(m,1,c)
+svm.fit(data_x,data_y,100)
+from mlxtend.plotting import plot_decision_regions
+x=np.linspace(-4,4,1000)
+test_x = np.array(np.meshgrid(x,x)).T.reshape(-1,2)
+test_y = svm.predict(test_x).reshape(-1).numpy()
+scatter_kwargs = {'alpha': 0.0}
+fig =plot_decision_regions(test_x, test_y, clf=svm,scatter_kwargs=scatter_kwargs)
+xx = np.linspace(-4,4,10)
+for i in range(svm.n_svm):
+    w = svm.get_w(i)
+    if w[1,0]==0:
+        plt.axvline(x=(-svm.b[i,0]/w[0,0]).item())
+    else:
+        k = (- w[0,0]/w[1,0]).item()
+        b = (- svm.b[i,0]/w[1,0]).item()
+        fig.plot(xx,k*xx+b)
+plt.show()
