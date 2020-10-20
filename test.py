b2fb8d310a01a15ba2a72def66401daf21319867
柴士童 <chaihahaha@qq.com>
2020-10-20 14:48:08 +0800

    fix test bug

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 from utils import *
-from svm_torch import svm_model_torch
+from svm_torch import *
 train_x, train_y = preprocessing(*(load("training_images.npy", "training_labels.npy")))
-svm = svm_model_torch(5000,10)
-svm.fit(train_x[:5000],train_y[:5000],1,kernel=grpf(10,3))
+n_train = 1000
+svm = svm_model_torch(n_train,10)
+svm.fit(train_x[:n_train],train_y[:n_train],1,kernel=grpf(10,3))
 val_x, val_y = preprocessing(*(load("validation_images.npy", "validation_labels.npy")))
 p_y = svm.predict(val_x)
 print(sum(p_y==val_y)/len(val_y))
